2da741dc85335b829376f9cab24ea6ebdc7eb9a4
arry-lee <arry_lee@qq.com>
2019-09-22 06:01:42 +0800

    modified wereader.py

--- client.py
+++ client.py
@@ -1,43 +1,43 @@
 from wereader import get_bookshelf,get_chapters,get_bookmarklist,get_bestbookmarks
 
 def main():
-	print('欢迎使用微信读书爬虫')
-	uid = input('请输入微信读书id:')
-	try:
-		if uid:
-			books = get_bookshelf(int(uid))
-		else:
-			books = get_bookshelf()
-	except:
-		print('请检查你的网络和Cookie设置')
-		return
+    print('欢迎使用微信读书爬虫')
+    uid = input('请输入微信读书id:')
+    try:
+        if uid:
+            books = get_bookshelf(int(uid))
+        else:
+            books = get_bookshelf()
+    except:
+        print('请检查你的网络和Cookie设置')
+        return
 
-	for b in books:
-		print(b.bookId,b.title,b.author)
+    for b in books:
+        print(b.bookId,b.title,b.author)
 
-	while True:
-		bid = input('请输入想看哪本书的目录，请输入对应id:')
+    while True:
+        bid = input('请输入想看哪本书的目录，请输入对应id:')
 
-		for c in get_chapters(int(bid)):
-			print('#'*c[0],c[1])
+        for c in get_chapters(int(bid)):
+            print('#'*c[0],c[1])
 
-		y =  input('是否需要查看该书的热门划线，y/n?')
-		if y.lower() == 'y':
-			bb = get_bestbookmarks(bid)
-			print(bb)
+        y =  input('是否需要查看该书的热门划线，y/n?')
+        if y.lower() == 'y':
+            bb = get_bestbookmarks(bid)
+            print(bb)
 
-		y =  input('是否需要查看你的笔记，y/n?')
-		if y.lower() == 'y':
-			bb = get_bookmarklist(bid)
-			print(bb)
+        y =  input('是否需要查看你的笔记，y/n?')
+        if y.lower() == 'y':
+            bb = get_bookmarklist(bid)
+            print(bb)
 
-		y = input('是否需要查看其他书，y/n?')
+        y = input('是否需要查看其他书，y/n?')
 
-		if y.lower() == 'y':
-			continue
-		else:
-			print('bye~')
-			break
+        if y.lower() == 'y':
+            continue
+        else:
+            print('bye~')
+            break
 
 main()
-	
+    
--- wereader.py
+++ wereader.py
@@ -2,13 +2,13 @@ import json
 import requests
 from collections import namedtuple,defaultdict
 from operator import itemgetter
+from itertools import chain
 
 from settings import COOKIE,USERVID
 
 requests.packages.urllib3.disable_warnings()
 
 Book = namedtuple('Book',['bookId','title','author','cover','category'])
-
 
 headers=\
 """
@@ -20,186 +20,138 @@ Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/a
 Accept-Encoding: gzip, deflate, br
 Accept-Language: zh-CN,zh;q=0.9,en;q=0.8
 """
-
-
 headers = dict(x.split(': ',1) for x in headers.splitlines() if x)
-
 headers.update(Cookie=COOKIE)
-# print(headers)
 
 def get_bookmarklist(bookId):
-	"""获取某本书的笔记返回md文本"""
-	url = "https://i.weread.qq.com/book/bookmarklist" 
-	
-	params = dict(bookId=bookId)
+    """获取某本书的笔记返回md文本"""
+    url = "https://i.weread.qq.com/book/bookmarklist" 
+    params = dict(bookId=bookId)
+    r = requests.get(url,params=params,headers=headers,verify=False)
 
-	try:
-		r = requests.get(url,params=params,headers=headers,verify=False)
-	except:
-		raise
+    if r.ok:
+        data = r.json()
+    else:
+        raise Exception(r.text)
+    chapters = {c['chapterUid']:c['title'] for c in data['chapters']}
+    contents = defaultdict(list)
 
-	if r.ok:
-		data = r.json()
-	else:
-		raise Exception(r.text)
+    for item in data['updated']:
+        chapter = item['chapterUid']
+        text = item['markText']
+        create_time = item["createTime"]
+        contents[chapter].append(text)
 
+    res = ''
+    for c in chapters:
+        title = chapters[c]
+        res += '## '+title+'\n'
+        for text in contents[c]:
+            res += '### '+text.strip()+'\n'
+        res += '\n'
 
-
-	#keys = ['synckey', 'totalCount', 'items', 'chapters']
-	chapters = {c['chapterUid']:c['title'] for c in data['chapters']}
-	contents = defaultdict(list)
-
-	for item in data['updated']:
-		chapter = item['chapterUid']
-		text = item['markText']
-		create_time = item["createTime"]
-		contents[chapter].append(text)
-
-	# print(contents)
-	res = ''
-	for c in chapters:
-		title = chapters[c]
-		res += '## '+title+'\n'
-		for text in contents[c]:
-			res += '### '+text.strip()+'\n'
-		res += '\n'
-
-	return res
-
+    return res
 
 def get_bestbookmarks(bookId):
-	"""获取书籍的热门划线,返回文本"""
+    """获取书籍的热门划线,返回文本"""
+    url = "https://i.weread.qq.com/book/bestbookmarks" 
+    params = dict(bookId=bookId)
+    r = requests.get(url,params=params,headers=headers,verify=False)
+    if r.ok:
+        data = r.json()
+    else:
+        raise Exception(r.text)
+    chapters = {c['chapterUid']:c['title'] for c in data['chapters']}
+    contents = defaultdict(list)
+    for item in data['items']:
+        chapter = item['chapterUid']
+        text = item['markText']
+        contents[chapter].append(text)
 
-	url = "https://i.weread.qq.com/book/bestbookmarks" 
-	params = dict(bookId=bookId)
-
-	try:
-		r = requests.get(url,params=params,headers=headers,verify=False)
-	except:
-		raise
-
-	if r.ok:
-		data = r.json()
-	else:
-		raise Exception(r.text)
-
-	#keys = ['synckey', 'totalCount', 'items', 'chapters']
-	chapters = {c['chapterUid']:c['title'] for c in data['chapters']}
-	contents = defaultdict(list)
-	for item in data['items']:
-		chapter = item['chapterUid']
-		text = item['markText']
-		contents[chapter].append(text)
-
-	res = ''
-	for c in chapters:
-		title = chapters[c]
-		res += '## '+title+'\n'
-		for text in contents[c]:
-			res += '### '+text.strip()+'\n'
-		res += '\n'
-
-	return res
+    res = ''
+    for c in chapters:
+        title = chapters[c]
+        res += '## '+title+'\n'
+        for text in contents[c]:
+            res += '### '+text.strip()+'\n'
+        res += '\n'
+    return res
 
 
 def get_chapters(bookId):
-	"""获取书的目录"""
-	url = "https://i.weread.qq.com/book/chapterInfos"
-	data = '{"bookIds":["%d"],"synckeys":[0]}'% bookId
-	# print(data)
-	try:
-		r = requests.post(url,data=data,headers=headers,verify=False)
-	except:
-		raise
+    """获取书的目录"""
+    url = "https://i.weread.qq.com/book/chapterInfos"
+    data = '{"bookIds":["%d"],"synckeys":[0]}'% bookId
 
-	if r.ok:
-		data = r.json()
-		# print(r.json())
-	else:
-		raise Exception(r.text)
+    r = requests.post(url,data=data,headers=headers,verify=False)
 
-	chapters = []
-	for item in data['data'][0]['updated']:
-		chapters.append((item['level'],item['title']))
+    if r.ok:
+        data = r.json()
+    else:
+        raise Exception(r.text)
 
-		if hasattr(item,'anchors'):
-			for ac in item['anchors']:
-				chapters.append((ac['level'],ac['title']))
-
-	return chapters
-
+    chapters = []
+    for item in data['data'][0]['updated']:
+        chapters.append((item['level'],item['title']))
+        if hasattr(item,'anchors'):
+            for ac in item['anchors']:
+                chapters.append((ac['level'],ac['title']))
+    return chapters
 
 def get_bookinfo(bookId):
-	url = "https://i.weread.qq.com/book/info" 
-	params = dict(bookId=bookId)
+    """获取书的详情"""
+    url = "https://i.weread.qq.com/book/info" 
+    params = dict(bookId=bookId)
+    r = requests.get(url,params=params,headers=headers,verify=False)
 
-	try:
-		r = requests.get(url,params=params,headers=headers,verify=False)
-	except:
-		raise
-
-	if r.ok:
-		data = r.json()
-	else:
-		raise Exception(r.text)
-
-	return data
+    if r.ok:
+        data = r.json()
+    else:
+        raise Exception(r.text)
+    return data
 
 def get_bookshelf(userVid=USERVID):
-	url = "https://i.weread.qq.com/shelf/friendCommon" 
-	params = dict(userVid=userVid)
-
-	try:
-		r = requests.get(url,params=params,headers=headers,verify=False)
-	except:
-		raise
-
-	if r.ok:
-		data = r.json()
-	else:
-		raise Exception(r.text)
-	from itertools import chain
-	books = []
-	for book in chain(data['finishReadBooks'],data['recentBooks']):
-		# print(i,book['title'],book['author'],book['price'],book['category'])
-		b = Book(book['bookId'],book['title'],book['author'],book['cover'],book['category'])
-		books.append(b)
-	books.sort(key=itemgetter(-1))
-	return books
+    """获取书架上所有书"""
+    url = "https://i.weread.qq.com/shelf/friendCommon" 
+    params = dict(userVid=userVid)
+    r = requests.get(url,params=params,headers=headers,verify=False)
+    if r.ok:
+        data = r.json()
+    else:
+        raise Exception(r.text)
+    books = []
+    for book in chain(data['finishReadBooks'],data['recentBooks']):
+        b = Book(book['bookId'],book['title'],book['author'],book['cover'],book['category'])
+        books.append(b)
+    books.sort(key=itemgetter(-1))
+    return books
 
 def get_notebooklist():
-	"""获取笔记书单"""
-	url = "https://i.weread.qq.com/user/notebooks"
-	try:
-		r = requests.get(url,headers=headers,verify=False)
-	except:
-		raise
+    """获取笔记书单"""
+    url = "https://i.weread.qq.com/user/notebooks"
+    r = requests.get(url,headers=headers,verify=False)
 
-	if r.ok:
-		data = r.json()
-	else:
-		raise Exception(r.text)
-	books = []
-	i = 0
-	for b in data['books']:
-		book = b['book']
-		i += 1
-		# print(i,book['title'],book['author'],book['price'],book['category'])
-		b = Book(book['bookId'],book['title'],book['author'],book['cover'],book['category'])
-		books.append(b)
-	books.sort(key=itemgetter(-1))
-	return books
+    if r.ok:
+        data = r.json()
+    else:
+        raise Exception(r.text)
+    books = []
+    for b in data['books']:
+        book = b['book']
+        b = Book(book['bookId'],book['title'],book['author'],book['cover'],book['category'])
+        books.append(b)
+    books.sort(key=itemgetter(-1))
+    return books
 
 
 
 if __name__ == '__main__':
-	main()
-	print(get_bookmarklist(680309))
-	books = get_booklist()
-	for b in books:
-		print(b)
-	print(get_bookinfo(680309))
-	for c in get_chapters(680309):
-		print('#'*c[0],c[1])
-	for b in get_bookshelf():
-		print(b)
+    print(get_bookmarklist(680309))
+    books = get_booklist()
+    for b in books:
+        print(b)
+    print(get_bookinfo(680309))
+    for c in get_chapters(680309):
+        print('#'*c[0],c[1])
+    for b in get_bookshelf():
+        print(b)
