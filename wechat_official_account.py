e72d671c9e2cda7f5a67610525f5fcb9aac32f1c
louie <727057301@qq.com>
2019-12-08 16:38:29 +0800

    note: nothing to changes

--- wechat_official_account.py
+++ wechat_official_account.py
@@ -7,6 +7,7 @@ def get_wechat(bookId):
     """获取书的详情"""
     headers = {
        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+        #  wr_vid=377902464; wr_skey=IwHc_Q9g;
         "Cookie": COOKIE,
     }
     url = "https://weread.qq.com/wrpage/book/share/%s?ref=app" % bookId
