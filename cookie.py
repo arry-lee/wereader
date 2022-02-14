ccf107934f23202015f2db853530518e1043799e
arry-lee <arry_lee@qq.com>
2022-02-14 16:20:29 +0800

    feature: 新增 GUI 版本的客户端，实现扫码登录逻辑

---
+++ cookie.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class ReadSqlite(object):
+    def __init__(self, path):
+        self.conn = sqlite3.connect(path)
+        self.cursor = self.conn.cursor()
+        self.cookies = dict()
+
+    def read_cookie(self):
+        try:
+            self.cursor.execute('select name,value from cookies;')
+            cookies = self.cursor.fetchall()
+            for key, value in cookies:
+                self.cookies[key] = value
+        except Exception:
+            raise FileNotFoundError('Cookie Not Found')
+        self.cursor.close()
+        self.conn.close()
+        return self.cookies
+
+
+def read_cookie_from_path(path):
+    sqlite = ReadSqlite(path)
+    return sqlite.read_cookie()
+
